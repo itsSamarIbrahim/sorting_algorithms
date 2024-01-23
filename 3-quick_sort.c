3f8db8de49b11b490fa233d6a71e1b9244109fa0
itsSamarIbrahim <>
2024-01-23 12:52:07 -0800

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -27,11 +27,13 @@ int partition(int *array, int low, int high, size_t size)
 			print_array(array, size);
 		}
 	}
-	temp = array[i + 1];
-	array[i + 1] = array[high];
-	array[high] = temp;
-	print_array(array, size);
-
+	if (array[i + 1] > array[high])
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
 	return (i + 1);
 }
 
