df070ba67c2547ed601bb281e9a8865b08a95ce0
itsSamarIbrahim <>
2024-01-23 16:14:53 -0800

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -21,19 +21,23 @@ int partition(int *array, int low, int high, size_t size)
 		if (array[j] < pivot)
 		{
 			i++;
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-			print_array(array, size);
+			if (array[i] != array[j])
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
 		}
 	}
-	if (array[i + 1] > array[high])
+	if (array[i + 1] != array[high])
 	{
 		temp = array[i + 1];
 		array[i + 1] = array[high];
 		array[high] = temp;
 		print_array(array, size);
 	}
+
 	return (i + 1);
 }
 
